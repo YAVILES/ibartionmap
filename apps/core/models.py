ad23abc876beb7f24caa7c15e9736ec6eae9cb1a
Yonathan Aviles <yonathanaviles123@gmail.com>
2021-10-23 12:00:48 -0400

    Semilla

---
+++ apps/core/models.py
@@ -0,0 +1,31 @@
+import uuid
+from django.db import models, IntegrityError
+from django.utils.translation import ugettext_lazy as _
+
+MONDAY = 0
+TUESDAY = 1
+WEDNESDAY = 2
+THURSDAY = 3
+FRIDAY = 4
+SATURDAY = 5
+SUNDAY = 6
+
+DAYS = (
+    (MONDAY, _('Lunes')),
+    (TUESDAY, _('Martes')),
+    (WEDNESDAY, _('Miercoles')),
+    (THURSDAY, _('Jueves')),
+    (FRIDAY, _('Viernes')),
+    (SATURDAY, _('Sábado')),
+    (SUNDAY, _('Domingo')),
+)
+
+
+class ModelBase(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    created = models.DateTimeField(verbose_name=_('created'), auto_now_add=True)
+    updated = models.DateTimeField(verbose_name=_('updated'), auto_now=True)
+
+    class Meta:
+        abstract = True
