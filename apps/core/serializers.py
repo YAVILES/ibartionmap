b5ab36ffa8fb94d07a19303a980766015abcc76c
Yonathan Aviles <yonathanaviles123@gmail.com>
2021-12-27 22:48:44 -0400

    Correccion en generacion de relaciones

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -20,7 +20,7 @@ class SynchronizedTablesDefaultSerializer(DynamicFieldsMixin, serializers.ModelS
                     data = [
                         d_two for d_two in relation.table_two.data
                         if d[relation.property_table_one] == d_two[relation.property_table_two]
-                    ][0]
+                    ]
                     if len(data) > 0:
                         d[relation.table_two.table] = data[0]
 
